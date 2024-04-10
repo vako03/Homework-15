99ec2ac2fc63772118088eebc40cb3b087de9e23
Vako <98945500+vako03@users.noreply.github.com>
2024-04-10 14:31:00 +0400

    Initial Commit

---
+++ Homework 15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 15
+//
+//  Created by vako on 10.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
